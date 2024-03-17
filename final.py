f4cd600e989ed1d8fcbf89cf83b9b1aede2a351d
bunny2ritik <120122959+bunny2ritik@users.noreply.github.com>
2024-03-18 00:51:53 +0530

    final.py

--- final.py
+++ final.py
@@ -85,8 +85,8 @@ def style_feedback_form(complaint_id):
     logo_image = "https://github.com/bunny2ritik/Utl-feedback/blob/main/newlogo.png?raw=true"  # Path to your logo image
     st.image(logo_image, use_column_width=True, width=400)
     
-    # Display the title for the complaint ID
-    st.markdown(f"<h3 style='text-align: center;'>Feedback for Complaint ID {complaint_id}</h3>", unsafe_allow_html=True)
+    # Display the title for the complaint ID without quotation marks
+    st.markdown("<h3 style='text-align: center;'>Feedback for Complaint ID : " + complaint_id + "</h3>", unsafe_allow_html=True)
 
     # Set title for service engineer section
     st.header('Service Engineer ')
@@ -130,4 +130,3 @@ if submit_button:
     # Submit feedback and handle API request
     if complaint_id:
         submit_feedback(complaint_id, engineer_review, coordinator_review)
-
